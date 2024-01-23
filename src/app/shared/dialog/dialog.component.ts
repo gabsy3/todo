e566784f4395cabc648971403cb82aecebc45646
gaby <gabybuchnik@gmail.com>
2024-01-23 15:50:51 +0200

    Todo

--- src/app/shared/dialog/dialog.component.ts
+++ src/app/shared/dialog/dialog.component.ts
@@ -9,7 +9,7 @@ import {
   MatDialogClose,
 } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ITodo } from '../../models/todo.model';
@@ -26,20 +26,27 @@ import { ITodo } from '../../models/todo.model';
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
+    ReactiveFormsModule,
   ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss',
 })
 export class DialogComponent implements OnInit {
+  updateTodoForm = new FormGroup({
+    id: new FormControl('' ,Validators.required),
+    title: new FormControl('' ,Validators.required),
+    description: new FormControl('' ,Validators.required),
+    status: new FormControl('' ,Validators.required)
+  });
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ITodo
   ) {}
   ngOnInit(): void {
+    
   }
 
-  
   onNoClick(): void {
-    this.dialogRef.close();
-  }
+      this.dialogRef.close();
+    }
 }
