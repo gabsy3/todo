4b62a0bf0b868bad3dc3fb711ed8d40291cfa4b9
gaby <gabsy3@gmail.com>
2024-03-06 13:39:28 +0200

    Todo server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8000;
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, '/dist/browser/')));
+app.use(express.static(path.join(__dirname, '/dist/todo/browser/')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
