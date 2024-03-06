5f5bfe2d2679ab6677700b0fda7d911bc7784ab9
gaby <gabsy3@gmail.com>
2024-03-06 09:43:28 +0200

    Todo server

---
+++ server/dist/todo/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-K3OKDCTT.mjs";import"./chunk-JHVXCPTP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 8000;
+const path = require('path');
 
+
+app.use(express.static(path.join(__dirname, 'dist/todo/browser/')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
